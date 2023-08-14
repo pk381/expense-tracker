86fdf835c9016f5ae4712ff60b814ac1079cea92
pk381 <kumarprabhat0023@gmail.com>
2023-08-14 22:39:25 +0530

    sending email through SIB

--- controllers/forgot_password.js
+++ controllers/forgot_password.js
@@ -14,5 +14,28 @@ exports.getForgotPassword = async (req, res, next)=>{
 
 exports.postForgotPassword = async (req, res, next)=>{
 
+    const client = Sib.ApiClient.instance;
+    const apiKey = client.authentications['api-key'];
+
+    apiKey.apiKey = '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';
+
+    const tranEmailApi = new Sib.TransactionalEmailsApi();
+
+    const sender = {email: 'kumarprabhat0023@gmail.com'};
+    const receivers = [{email: 'prabhat_b190521cs@nitc.ac.in'}];
+
+    tranEmailApi.sendTransacEmail({
+       sender,
+       to: receivers,
+       subject: 'forgot password',
+       textContent: `reset your password`
+    })
+    .then(res=>{
+        console.log(res);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+
     console.log(req.body.email);
 }
